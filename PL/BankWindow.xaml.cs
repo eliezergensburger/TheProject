38f583f6fcc1ae66a06fb7e1d2d0ae7f930c239e
eliezer gensburger <eliezer.jct@gmail.com>
2020-01-29 21:13:29 +0200

    BankWindow with datagrid combobox progessbar backgroundworker HttpRequest bank of israel

--- PL/BankWindow.xaml.cs
+++ PL/BankWindow.xaml.cs
@@ -29,6 +29,8 @@ namespace PL
         public BankWindow()
         {
             InitializeComponent();
+            comboBoxBanks.ItemsSource = null;
+
             bg.DoWork += Bg_DoWork;
             bg.RunWorkerCompleted += Bg_RunWorkerCompleted;
             bg.ProgressChanged += Bg_ProgressChanged;
@@ -37,13 +39,18 @@ namespace PL
 
         private void Bg_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-           this.progressBar.Value = e.ProgressPercentage;
+            this.progressBar.Value = e.ProgressPercentage;
         }
 
         private void Bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            DataView dv = e.Result as DataView;
+            DataTable dt = e.Result as DataTable;
+            DataView dv = dt.DefaultView;
+
             this.banksDatagrid.DataContext = dv;
+
+            var banknames = dv.ToTable(true, "Bank_Name").AsEnumerable();
+            comboBoxBanks.ItemsSource = banknames.Select(dr => dr["Bank_Name"].ToString().Trim()).Distinct();
         }
 
         private void Bg_DoWork(object sender, DoWorkEventArgs e)
@@ -54,23 +61,46 @@ namespace PL
             httpRequest = (HttpWebRequest)WebRequest.Create(uriString);
             httpRequest.Method = WebRequestMethods.Http.Get;
 
-            bg.ReportProgress(20);
-             // Get back the HTTP response for web server
+            bg.ReportProgress(30);
+            // Get back the HTTP response for web server
             HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse();
             Stream httpResponseStream = httpResponse.GetResponseStream();
 
-            bg.ReportProgress(50);
-            // Construct HTTP request to get the logo
+            bg.ReportProgress(40);
+            // Construct the Datset and read the data from the stream
 
             DataSet ds = new DataSet();
             ds.ReadXml(httpResponseStream);
-            e.Result = ds.Tables[0].DefaultView;
+
+            bg.ReportProgress(90);
+            DataTable dt = ds.Tables[0];
+            e.Result = dt;
+
             bg.ReportProgress(100);
         }
 
         private void populateBanksBtn_Click(object sender, RoutedEventArgs e)
         {
             bg.RunWorkerAsync(@"https://www.boi.org.il/en/BankingSupervision/BanksAndBranchLocations/Lists/BoiBankBranchesDocs/snifim_en.xml");
+        }
+
+        private void comboBoxBanks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBoxBanks.ItemsSource != null)
+            {
+                string str = comboBoxBanks.SelectedItem as String;
+  
+                foreach (DataRowView drv in (DataView)banksDatagrid.ItemsSource)
+                {
+                    if (drv["Bank_Name"].ToString() == str)
+                    {
+                        // This is the data row view record you want...
+                        banksDatagrid.SelectedItem = drv;
+                        banksDatagrid.ScrollIntoView(drv);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
